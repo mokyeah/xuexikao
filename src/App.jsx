c6b7e67dd4813e28aa541f2ce36296dae34a09fe
mokyeah <mzmmzm2008@gmail.com>
2022-09-23 21:52:44 +0800

    cz

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
+import moment from "moment";
+import { Icon } from '@iconify/react';
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
